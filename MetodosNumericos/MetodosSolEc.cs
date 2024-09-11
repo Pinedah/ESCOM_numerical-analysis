ad8b1cbd11f347dce26096d6effc6837244fdfae
Rodrigo Ramirez <ramirez.aguilar.rodrigo.vidal@gmail.com>
2024-09-10 22:08:28 -0600

    Update MetodosSolEc.cs
    Se agrego metodo muller 2.0

--- MetodosNumericos/MetodosSolEc.cs
+++ MetodosNumericos/MetodosSolEc.cs
@@ -14,6 +14,7 @@ namespace MetodosNumericos
     {
         public int numMaxIter;
         public float errorMaximo;
+        public Complex errorMaximoComplex;
 
         public bool metBiseccion(float a, float b)
         { // Metodo de biseccion
@@ -49,24 +50,27 @@ namespace MetodosNumericos
 
         public bool metMuller(Complex p0, Complex p1,Complex  p2)
         { // Metodo de biseccion
-            float errorActual;
+            Complex errorActual;
             int i;
             i = 1;
             Complex D,a,b,c;
+            Complex p3;
+            Complex negativo = new Complex(0, 0);
             while (i <= numMaxIter)
             {
                 D=(p1-p2)*(p0-p2)*(p0-p2);
-                a = ((p1-p2));
-                errorActual = (b - a) / 2;
-                if (errorActual <= errorMaximo)
+                a = ((p1-p2)*(FuncCom(p0)-FuncCom(p2))- (p0 - p2) * (FuncCom(p1) - FuncCom(p2)))/D;
+                b = ((Complex.Pow(p0-p2,2)*(FuncCom(p1)- FuncCom(p2))) - (Complex.Pow(p1 - p2, 2) * (FuncCom(p0) - FuncCom(p2)))) / D;
+                c = FuncCom(p2);
+
+                p3 = p2 - (2 * c / (b + (b.Magnitude < negativo.Magnitude ? -1 : 1)) * Complex.Sqrt(Complex.Pow(b, 2) - 4 * a * c));
+                errorActual = Complex.Abs(p3-p2);
+                if (errorActual.Magnitude <= errorMaximoComplex.Magnitude)
                 {
                     MessageBox.Show("Se obvtuvo la aproximacion a la raiz con el error deseado. \nRaiz = " + c.ToString(), "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     return true;
                 }
-                if (Func(a) * Func(c) < 0)
-                    b = c;
-                else
-                    a = c;
+                p0=p1; p1 = p2; p2 = p3;
 
                 i++;
             }
@@ -83,7 +87,7 @@ namespace MetodosNumericos
         Complex FuncCom(Complex x)
         {
             Complex r;
-            r = (Complex)(Complex.Pow(x,2) - 3.0);
+            r = (Complex)(Complex.Pow(x,2) - 3);
             return r;
         }
 
