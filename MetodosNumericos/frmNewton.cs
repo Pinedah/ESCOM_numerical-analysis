aa16e3b5a564fea5337ba22fc8fc5776554084ff
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-13 07:56:34 -0600

    Metodo de newton agregado w

---
+++ MetodosNumericos/frmNewton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodosNumericos
+{
+    public partial class frmNewton : Form
+    {
+        public frmNewton()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+
+            float po, errorMax;
+            int numMaxIte;
+            bool res;
+
+            po = float.Parse(txt_po.Text);
+            errorMax = float.Parse(ErrMax.Text);
+            numMaxIte = int.Parse(numMaxIter.Text);
+
+
+            MetodosSolEc metodosSolEc = new MetodosSolEc();
+
+            metodosSolEc.errorMaximo = errorMax;
+            metodosSolEc.numMaxIter = numMaxIte;
+
+            metodosSolEc.metNewton(po, ref this.dgv_result);
+
+        }
+    }
+}
