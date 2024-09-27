d153ab6a11f460d1cd6979489ad182ce8978ba7c
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-26 22:26:38 -0600

    arreglar vista biseccion aitken

--- MetodosNumericos/frmBiseccionAitken.Designer.cs
+++ MetodosNumericos/frmBiseccionAitken.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgv_result = new System.Windows.Forms.DataGridView();
             this.btn_calcular = new System.Windows.Forms.Button();
             this.tf_b = new System.Windows.Forms.TextBox();
@@ -45,16 +45,16 @@
             // 
             // dgv_result
             // 
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgv_result.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_result.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dgv_result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_result.Location = new System.Drawing.Point(80, 163);
+            this.dgv_result.Location = new System.Drawing.Point(16, 163);
             this.dgv_result.Name = "dgv_result";
             this.dgv_result.Size = new System.Drawing.Size(645, 269);
             this.dgv_result.TabIndex = 21;
@@ -63,7 +63,7 @@
             // 
             this.btn_calcular.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btn_calcular.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_calcular.Location = new System.Drawing.Point(602, 18);
+            this.btn_calcular.Location = new System.Drawing.Point(538, 18);
             this.btn_calcular.Name = "btn_calcular";
             this.btn_calcular.Size = new System.Drawing.Size(123, 39);
             this.btn_calcular.TabIndex = 20;
@@ -74,7 +74,7 @@
             // tf_b
             // 
             this.tf_b.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tf_b.Location = new System.Drawing.Point(625, 118);
+            this.tf_b.Location = new System.Drawing.Point(561, 118);
             this.tf_b.Name = "tf_b";
             this.tf_b.Size = new System.Drawing.Size(100, 29);
             this.tf_b.TabIndex = 19;
@@ -83,7 +83,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(598, 123);
+            this.label5.Location = new System.Drawing.Point(534, 123);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(25, 22);
             this.label5.TabIndex = 18;
@@ -92,7 +92,7 @@
             // tf_a
             // 
             this.tf_a.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tf_a.Location = new System.Drawing.Point(625, 79);
+            this.tf_a.Location = new System.Drawing.Point(561, 79);
             this.tf_a.Name = "tf_a";
             this.tf_a.Size = new System.Drawing.Size(100, 29);
             this.tf_a.TabIndex = 17;
@@ -101,7 +101,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(598, 82);
+            this.label4.Location = new System.Drawing.Point(534, 82);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(24, 22);
             this.label4.TabIndex = 16;
@@ -110,7 +110,7 @@
             // ErrMax
             // 
             this.ErrMax.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ErrMax.Location = new System.Drawing.Point(414, 120);
+            this.ErrMax.Location = new System.Drawing.Point(350, 120);
             this.ErrMax.Name = "ErrMax";
             this.ErrMax.Size = new System.Drawing.Size(100, 29);
             this.ErrMax.TabIndex = 15;
@@ -119,7 +119,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(76, 125);
+            this.label3.Location = new System.Drawing.Point(12, 125);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(130, 22);
             this.label3.TabIndex = 14;
@@ -128,7 +128,7 @@
             // numMaxIter
             // 
             this.numMaxIter.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numMaxIter.Location = new System.Drawing.Point(414, 79);
+            this.numMaxIter.Location = new System.Drawing.Point(350, 79);
             this.numMaxIter.Name = "numMaxIter";
             this.numMaxIter.Size = new System.Drawing.Size(100, 29);
             this.numMaxIter.TabIndex = 13;
@@ -137,7 +137,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(76, 82);
+            this.label2.Location = new System.Drawing.Point(12, 82);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(268, 22);
             this.label2.TabIndex = 12;
@@ -147,7 +147,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(76, 18);
+            this.label1.Location = new System.Drawing.Point(12, 18);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(329, 22);
@@ -159,7 +159,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumPurple;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(684, 461);
             this.Controls.Add(this.dgv_result);
             this.Controls.Add(this.btn_calcular);
             this.Controls.Add(this.tf_b);
