db22cf4c701d1d0f7129abd93d1e49388fcb9836
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-28 20:10:52 -0600

    arreglar detalle en la vista del punto 2

--- MetodosNumericos/frmDeflacion.Designer.cs
+++ MetodosNumericos/frmDeflacion.Designer.cs
@@ -62,7 +62,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calisto MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(355, 220);
+            this.label6.Location = new System.Drawing.Point(344, 220);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(37, 22);
             this.label6.TabIndex = 35;
