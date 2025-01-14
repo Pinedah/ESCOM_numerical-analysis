8bbbd0bdbc5a66a7e21d5b92eca7d28946eb1d12
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2025-01-13 22:24:06 -0600

    arreglar formulas de derivacion

--- MetodosNumericos/Derivacion.cs
+++ MetodosNumericos/Derivacion.cs
@@ -10,28 +10,29 @@ namespace MetodosNumericos
     {
         public float x0;
         public float h;
-        public float Derivacion2Puntos(float x0,float h)
+        public float Derivacion2Puntos(float x0, float h)
         {
-
-            float derivada = (f(x0+h)-f(x0))/h;
+            float derivada = (f(x0 + h) - f(x0)) / h; 
             return derivada;
         }
         public float Derivacion3Puntos(float x0, float h)
         {
-
-            float derivada = (-3*f(x0)+4*f(x0+h)-f(x0+2*h)) / 2*h;
+            float derivada = (-3 * f(x0) + 4 * f(x0 + h) - f(x0 + 2 * h)) / (2 * h); 
             return derivada;
         }
         public float Derivacion4Puntos(float x0, float h)
         {
-
-            float derivada = (-11 * f(x0) + 18 * f(x0 + h) - 9 * f(x0 + 2 * h) + 2 * f(x0 +3*h)) / 6 * h;
+            float derivada = (-11 * f(x0) + 18 * f(x0 + h) - 9 * f(x0 + 2 * h) + 2 * f(x0 + 3 * h)) / (6 * h); 
             return derivada;
         }
-        float f(float x){
+        float f(float x)
+        {
             float r;
             r = (float)(x * Math.Sin(Math.Sqrt(x * x)));
             return r;
         }
+
+
+
     }
 }
